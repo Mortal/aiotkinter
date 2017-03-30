dd73e40e3f3f233a9d0acdb1fa259ca38ded2733
Mathias Rav <rav@cs.au.dk>
2017-03-30 17:15:30 -0400

    Implement asynchronous askyesno

--- test.py
+++ test.py
@@ -82,6 +82,75 @@ def sigint_wrapper(fn):
     return lambda: run_in_thread(fn)
 
 
+class Dialog(tkinter.Toplevel):
+    # Based on http://effbot.org/tkinterbook/tkinter-dialog-windows.htm
+    def __init__(self, loop, parent, title, message):
+        super().__init__(parent)
+        self.transient(parent)
+        self.title(title)
+        self.parent = parent
+        self.result = None
+        body = tkinter.Frame(self)
+        self.initial_focus = self.body(body, message)
+        body.pack(padx=5, pady=5)
+        self.buttonbox()
+        self.grab_set()
+        if not self.initial_focus:
+            self.initial_focus = self
+        self.protocol("WM_DELETE_WINDOW", self.cancel)
+        self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
+                                  parent.winfo_rooty()+50))
+        self.initial_focus.focus_set()
+        self.result = asyncio.Future(loop=loop)
+
+    def body(self, master, message: str):
+        tkinter.Label(master, text=message).grid(row=0)
+
+    def buttonbox(self):
+        box = tkinter.Frame(self)
+
+        w = tkinter.Button(box, text="OK", width=10, command=self.ok,
+                           default=tkinter.ACTIVE)
+        w.pack(side=tkinter.LEFT, padx=5, pady=5)
+        w = tkinter.Button(box, text="Cancel", width=10, command=self.cancel)
+        w.pack(side=tkinter.LEFT, padx=5, pady=5)
+
+        self.bind("<Return>", self.ok)
+        self.bind("<Escape>", self.cancel)
+
+        box.pack()
+
+    def ok(self, event=None):
+        if not self.validate():
+            self.initial_focus.focus_set()
+            return
+
+        self.withdraw()
+        self.update_idletasks()
+        self.apply()
+        self.close()
+        self.result.set_result(True)
+
+    def cancel(self, event=None):
+        self.result.set_result(False)
+        self.close()
+
+    def close(self):
+        self.parent.focus_set()
+        self.destroy()
+
+    def validate(self):
+        return True
+
+    def apply(self):
+        pass
+
+
+async def askyesno(title, message, parent, loop):
+    d = Dialog(loop, parent, title, message)
+    return await d.result
+
+
 @sigint_wrapper
 def main(sigint_handler):
     loop = aiotkinter.TkinterEventLoopPolicy().new_event_loop()
@@ -102,7 +171,7 @@ def main(sigint_handler):
         loop.stop()
 
     async def quit_action():
-        if tkinter.messagebox.askyesno('Quit?', 'Really quit?', master=root):
+        if await askyesno('Quit?', 'Really quit?', root, loop):
             really_quit()
 
     root.protocol("WM_DELETE_WINDOW", async_cb(quit_action, loop))

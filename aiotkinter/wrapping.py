0c929a79f9f4d9190365c6aecaa764b236989cdf
Mathias Rav <rav@cs.au.dk>
2017-03-31 11:22:07 -0400

    Move everything into a class

--- aiotkinter/wrapping.py
+++ aiotkinter/wrapping.py
@@ -11,33 +11,43 @@ class WrappedException(Exception):
     pass
 
 
-def run_in_thread(function, max_ignored=1):
-    tkinter.NoDefaultRoot()
+class SigintHandler:
+    def __init__(self, function=None, *, max_ignored=1):
+        self.max_ignored = max_ignored
+        self._keyboard_interrupt = None
+        self._quit_r, self._quit_w = os.pipe()
+        self._callback = None
+        self._ignored = 0
+        self._uncaught_exception = None
+        if function is not None:
+            self.execute(function)
 
-    quit_r, quit_w = os.pipe()
+    def propagate_keyboard_interrupt(self, exn):
+        if self._keyboard_interrupt is not None:
+            # This is not the first exception; previous was ignored.
+            self._ignored += 1
+            if self.max_ignored and self._ignored >= self.max_ignored:
+                raise
+        self._keyboard_interrupt = exn
+        os.write(self._quit_w, b'x')
 
-    ignored = 0
-    keyboard_interrupt = None
+    def _quit_readable(self):
+        os.read(self._quit_r, 1)
+        if self._callback is None:
+            raise KeyboardInterrupt() from self._keyboard_interrupt
+        self._callback()
 
-    def sigint_handler(loop, cb=None):
-        def quit_readable():
-            os.read(quit_r, 1)
-            if cb is None:
-                raise KeyboardInterrupt() from keyboard_interrupt
-            cb()
+    def register_reader(self, loop, cb=None):
+        self._callback = cb
+        loop.add_reader(self._quit_r, self._quit_readable)
 
-        loop.add_reader(quit_r, quit_readable)
-
-    uncaught_exception = None
-
-    def wrapper():
+    def run(self, function):
         try:
-            function(sigint_handler)
+            function(self.register_reader)
         except Exception:
             # Use traceback.format_exc() to ensure that no tkinter
             # objects leak out of the thread due to the exception handler.
-            nonlocal uncaught_exception
-            uncaught_exception = traceback.format_exc()
+            self._uncaught_exception = traceback.format_exc()
         # Run GC collection to ensure that tkinter objects are collected in
         # this thread and not the main thread.
         try:
@@ -46,22 +56,29 @@ def run_in_thread(function, max_ignored=1):
         except ImportError:
             pass
 
-    # Set daemon=True to let the interpreter kill an un-cooperative thread
-    thread = threading.Thread(None, wrapper, daemon=True)
-    thread.start()
-    while True:
-        try:
-            thread.join()
-            break
-        except KeyboardInterrupt as exn:
-            if keyboard_interrupt is not None:
-                ignored += 1
-                if max_ignored and ignored >= max_ignored:
-                    raise
-            keyboard_interrupt = exn
-            os.write(quit_w, b'x')
-    if uncaught_exception is not None:
-        raise WrappedException(uncaught_exception)
+    def reraise_uncaught(self):
+        if self._uncaught_exception is not None:
+            raise WrappedException(self._uncaught_exception)
+
+    def create_thread(self, function):
+        # Set daemon=True to let the interpreter kill an un-cooperative thread
+        return threading.Thread(None, self.run, args=(function,), daemon=True)
+
+    def execute(self, function):
+        thread = self.create_thread(function)
+        thread.start()
+        while True:
+            try:
+                thread.join()
+                break
+            except KeyboardInterrupt as exn:
+                self.propagate_keyboard_interrupt(exn)
+        self.reraise_uncaught()
+
+
+def run_in_thread(function, max_ignored=1):
+    tkinter.NoDefaultRoot()
+    handler = SigintHandler(function, max_ignored=max_ignored)
 
 
 def threaded_sigint_wrapper(fn):
